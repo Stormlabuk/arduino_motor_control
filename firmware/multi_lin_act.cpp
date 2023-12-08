f87116b66db9d8d04f8d5eeb50883cc40576c0ee
Vittorio <vittorio.francescon@gmail.com>
2023-12-08 16:49:35 +0000

    working on hex bug

--- firmware/multi_lin_act.cpp
+++ firmware/multi_lin_act.cpp
@@ -1,6 +1,6 @@
-#include <Rosserial_Arduino_Library/src/ros.h>
-#include <Rosserial_Arduino_Library/src/std_msgs/UInt32.h>
-#include <Servo/Servo.h>
+#include <ros.h>
+#include <std_msgs/UInt32.h>
+#include <Servo.h>
 
 ros::NodeHandle nh;
 /**
@@ -8,19 +8,41 @@ ros::NodeHandle nh;
  * act 0 is the most proximal actuator, act 3 is the most distal
  */
 Servo act0, act1, act2, act3;
+char loginfo_buffer[100];
 
 void act_cb(const std_msgs::UInt32 &cmd_msg) {
     uint32_t data = cmd_msg.data;
-    int act_cmds[4];
+    uint8_t act_cmds[4];
+    int step_inputs[4];
+    sprintf(loginfo_buffer, "data, var: %X", data);
+    nh.loginfo(loginfo_buffer);
+
     for (int i = 0; i < 4; i++) {
-        act_cmds[i] = (int)(data & 0xFF) * 1000 / 255 +
-                      1000;  // Extract the rightmost byte
+        act_cmds[i] = (data & 0xFF);  // Extract the rightmost byte
+        step_inputs[i] = (int) (act_cmds[i]) * 1000 / 255 +
+                      1000;
+        sprintf(loginfo_buffer, "shifted data: %02X at i %d", act_cmds[i], i);
+        nh.loginfo(loginfo_buffer);
+        sprintf(loginfo_buffer, "calc data: %d at i %d", step_inputs[i], i);
+        nh.loginfo(loginfo_buffer);
         data >>= 8;
     }
-    act0.writeMicroseconds(act_cmds[3]);
-    act1.writeMicroseconds(act_cmds[2]);
-    act2.writeMicroseconds(act_cmds[1]);
-    act3.writeMicroseconds(act_cmds[0]);
+    
+
+
+    // sprintf(loginfo_buffer, "act0: %d", act_cmds[3]);
+    // nh.loginfo(loginfo_buffer);
+    // sprintf(loginfo_buffer, "act1: %d", act_cmds[2]);
+    // nh.loginfo(loginfo_buffer);
+    // sprintf(loginfo_buffer, "act2: %d", act_cmds[1]);
+    // nh.loginfo(loginfo_buffer);
+    // sprintf(loginfo_buffer, "act3: %d", act_cmds[0]);
+    // nh.loginfo(loginfo_buffer);
+
+    // act0.writeMicroseconds(act_cmds[3]);
+    // act1.writeMicroseconds(act_cmds[2]);
+    // act2.writeMicroseconds(act_cmds[1]);
+    // act3.writeMicroseconds(act_cmds[0]);
 }
 
 ros::Subscriber<std_msgs::UInt32> sub("servo", act_cb);
@@ -35,10 +57,10 @@ void setup() {
     act3.attach(12);
 
     // initialise positions
-    act0.writeMicroseconds(1100);
-    act1.writeMicroseconds(1100);
-    act2.writeMicroseconds(1100);
-    act3.writeMicroseconds(1100);
+    act0.writeMicroseconds(1500);
+    act1.writeMicroseconds(1500);
+    act2.writeMicroseconds(1500);
+    act3.writeMicroseconds(1500);
 }
 
 void loop() {
--- scripts/sampling_func.py
+++ scripts/sampling_func.py
@@ -5,54 +5,69 @@ from std_msgs.msg import Int32
 from ros_coils.msg import magField
 # from ros_coils.msg import magFieldArray
 
-class SamplingFunc:
 
+class SamplingFunc:
 
     def __init__(self):
         rospy.init_node('wave_sampling_node', anonymous=True)
 
         # Create publishers for the "servo" and "field" topics
         self.servo_pub = rospy.Publisher('servo', UInt32, queue_size=10)
-        self.field_pub = rospy.Publisher('field', magField, queue_size=10)     
+        self.field_pub = rospy.Publisher('field', magField, queue_size=10)
         self.stepper_pub = rospy.Publisher('stepper', Int32, queue_size=10)
 
         # Create a rosparam for ts
         self.ts = rospy.get_param('~ts', default=4)  # Default value is 4
-        if(self.ts < 3 or self.ts > 21):
-            if(self.ts % 2 == 0):
+        if (self.ts < 3 or self.ts > 21):
+            if (self.ts % 2 == 0):
                 self.ts = self.ts - 1
         freq = self.ts / 3
         self.rate = rospy.Rate(freq)  # 10 Hz
 
-        self.field_min = rospy.get_param('/sampler/field_min', default=[0, 0, 0])  # Default value is [0, 0, 0]
-        self.field_max = rospy.get_param('/sampler/field_max', default=[0, 0, 0])  # Default value is [0, 0, 0]
-        self.step_min = rospy.get_param('/sampler/step_min', default=[0, 0, 0, 0]) # Default value is [0, 0, 0, 0]
-        self.step_max = rospy.get_param('/sampler/step_max', default=[0, 0, 0, 0]) # Default value is [0, 0, 0, 0]
+        self.field_min = rospy.get_param(
+            '/sampler/field_min', default=[0, 0, 0])  # Default value is [0, 0, 0]
+        self.field_max = rospy.get_param(
+            '/sampler/field_max', default=[0, 0, 0])  # Default value is [0, 0, 0]
+        # Default value is [0, 0, 0, 0]
+        self.step_min = rospy.get_param(
+            '/sampler/step_min', default=[0, 0, 0, 0])
+        # Default value is [0, 0, 0, 0]
+        self.step_max = rospy.get_param(
+            '/sampler/step_max', default=[0, 0, 0, 0])
 
-        self.bxs = self.getInterval(self.ts, self.field_min[0], self.field_max[0])
-        self.bys = self.getInterval(self.ts, self.field_min[1], self.field_max[1])
-        self.bzs = self.getInterval(self.ts, self.field_min[2], self.field_max[2])
+        self.bxs = self.getInterval(
+            self.ts, self.field_min[0], self.field_max[0])
+        self.bys = self.getInterval(
+            self.ts, self.field_min[1], self.field_max[1])
+        self.bzs = self.getInterval(
+            self.ts, self.field_min[2], self.field_max[2])
         self.field_to_pub = np.array([self.bxs, self.bys, self.bzs])
-        
-        self.step_0 = self.getInterval(self.ts, self.step_min[0], self.step_max[0])
-        self.step_1 = self.getInterval(self.ts, self.step_min[1], self.step_max[1])
-        self.step_2 = self.getInterval(self.ts, self.step_min[2], self.step_max[2])
-        self.step_3 = self.getInterval(self.ts, self.step_min[3], self.step_max[3])
+
+        self.step_0 = self.getInterval(
+            self.ts, self.step_min[0], self.step_max[0])
+        self.step_1 = self.getInterval(
+            self.ts, self.step_min[1], self.step_max[1])
+        self.step_2 = self.getInterval(
+            self.ts, self.step_min[2], self.step_max[2])
+        self.step_3 = self.getInterval(
+            self.ts, self.step_min[3], self.step_max[3])
 
         # print("field_to_pub: ", self.field_to_pub)
 
     def getInterval(self, ts, min, max):
-        # print arguments        
-        dividend = int( np.floor( (ts+1) / 2) ) # Number of intervals
-        disc_interval = np.linspace(min, max, dividend) # discretised left hand of the curve
-        disc_interval = np.concatenate((disc_interval, disc_interval[::-1][1:])) # append right hand
+        # print arguments
+        dividend = int(np.floor((ts+1) / 2))  # Number of intervals
+        # discretised left hand of the curve
+        disc_interval = np.linspace(min, max, dividend)
+        disc_interval = np.concatenate(
+            (disc_interval, disc_interval[::-1][1:]))  # append right hand
         return disc_interval
 
     def assembleServoInput(self, idx):
-        s0 = int( self.step_0[idx] * 255 / 100)
-        s1 = int( self.step_1[idx] * 255 / 100)
-        s2 = int( self.step_2[idx] * 255 / 100)
-        s3 = int( self.step_3[idx] * 255 / 100)
+        s0 = int(self.step_0[idx] * 255 / 100)
+        s1 = int(self.step_1[idx] * 255 / 100)
+        s2 = int(self.step_2[idx] * 255 / 100)
+        s3 = int(self.step_3[idx] * 255 / 100)
         uint32_value = (s0 << 24) | (s1 << 16) | (s2 << 8) | s3
         msg = UInt32()
         msg.data = uint32_value
@@ -65,12 +80,12 @@ class SamplingFunc:
             servo_msg = UInt32()
             field_msg = magField()
 
-            if(stepperOut and i == 0):
+            if (stepperOut and i == 0):
                 stepper_msg = Int32()
                 stepper_msg.data = 50
                 self.stepper_pub.publish(stepper_msg)
                 stepperOut = False
-            elif(not stepperOut and i == 0):
+            elif (not stepperOut and i == 0):
                 stepper_msg = Int32()
                 stepper_msg.data = -50
                 self.stepper_pub.publish(stepper_msg)
@@ -80,9 +95,9 @@ class SamplingFunc:
             # print("field_to_pub: ", self.field_to_pub[:,i])
             servo_msg = self.assembleServoInput(i)
             # print("servo_msg: ", servo_msg)
-            field_msg.bx = self.field_to_pub[0,i]
-            field_msg.by = self.field_to_pub[1,i]
-            field_msg.bz = self.field_to_pub[2,i]
+            field_msg.bx = self.field_to_pub[0, i]
+            field_msg.by = self.field_to_pub[1, i]
+            field_msg.bz = self.field_to_pub[2, i]
 
             # Publish to the "servo" topic
             self.servo_pub.publish(servo_msg)
@@ -90,9 +105,10 @@ class SamplingFunc:
             self.field_pub.publish(field_msg)
 
             i = i + 1
-            if(i == self.ts):
-                i = 0
+            if (i == self.ts):
+                i = 1
             self.rate.sleep()
+
 
 if __name__ == '__main__':
     sampling_func = SamplingFunc()
