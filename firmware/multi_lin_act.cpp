53a59557dd158b633dbc8553fe5023aae3ad05f4
Vittorio <vittorio.francescon@gmail.com>
2023-12-11 12:08:22 +0000

    tested floating point fix. all clear

--- firmware/multi_lin_act.cpp
+++ firmware/multi_lin_act.cpp
@@ -1,6 +1,6 @@
+#include <Servo.h>
 #include <ros.h>
 #include <std_msgs/UInt32.h>
-#include <Servo.h>
 
 ros::NodeHandle nh;
 /**
@@ -15,22 +15,20 @@ void act_cb(const std_msgs::UInt32 &cmd_msg) {
     uint8_t extractedBytes[4];
     float conv[4];
     int step_inputs[4];
-    sprintf(loginfo_buffer, "data, var: %X", data);
-    nh.loginfo(loginfo_buffer);
+    // sprintf(loginfo_buffer, "data, var: %X", data);
+    // nh.loginfo(loginfo_buffer);
 
     for (int i = 0; i < 4; i++) {
-        extractedBytes[i] = data >> 8*(3-i) & 0xFF; 
-        conv[i] =  extractedBytes[i];
-        conv[i] = (float) conv[i] / 255;
+        extractedBytes[i] = data >> 8 * (3 - i) & 0xFF;
+        conv[i] = extractedBytes[i];
+        conv[i] = (float)conv[i] / 255;
         conv[i] = conv[i] * 1000;
-        step_inputs[i] = conv[i] * 1000;
-        sprintf(loginfo_buffer, "extracted at i=%d: %02X", i, extractedBytes[i]);
-        nh.loginfo(loginfo_buffer);
-        sprintf(loginfo_buffer, "calc data at i=%d: %d",i, step_inputs[i]);
-        nh.loginfo(loginfo_buffer);
+        step_inputs[i] = conv[i] + 1000;
+        // sprintf(loginfo_buffer, "extracted at i=%d: %02X", i,
+        // extractedBytes[i]); nh.loginfo(loginfo_buffer);
+        // sprintf(loginfo_buffer, "calc data at i=%d: %d",i, step_inputs[i]);
+        // nh.loginfo(loginfo_buffer);
     }
-    
-
 
     // sprintf(loginfo_buffer, "act0: %d", act_cmds[3]);
     // nh.loginfo(loginfo_buffer);
@@ -41,10 +39,10 @@ void act_cb(const std_msgs::UInt32 &cmd_msg) {
     // sprintf(loginfo_buffer, "act3: %d", act_cmds[0]);
     // nh.loginfo(loginfo_buffer);
 
-    // act0.writeMicroseconds(act_cmds[3]);
-    // act1.writeMicroseconds(act_cmds[2]);
-    // act2.writeMicroseconds(act_cmds[1]);
-    // act3.writeMicroseconds(act_cmds[0]);
+    act0.writeMicroseconds(step_inputs[3]);
+    act1.writeMicroseconds(step_inputs[2]);
+    act2.writeMicroseconds(step_inputs[1]);
+    act3.writeMicroseconds(step_inputs[0]);
 }
 
 ros::Subscriber<std_msgs::UInt32> sub("servo", act_cb);
